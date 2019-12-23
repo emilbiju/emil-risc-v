5c21c45ff8e768c80790ddad8a89a88c2cab02c6
Emil Biju <39640592+emilbiju@users.noreply.github.com>
2019-12-23 22:00:48 +0530

    Add files via upload
    Pipelined RISC-V CPU implementation with branch predictor for greater processing speed

---
+++ Pipelined_Branch_Predict/cpu_tb.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+
+module pipeline_cpu_tb();
+    reg clk, reset;     //check.. drdata and idata should be reg??????
+    wire [31:0] iaddr;
+    wire [31:0] x31;
+	 wire [31:0] L_regdata_out;
+	 wire [31:0] EM_daddr_out;
+	 wire [31:0] MW_drdata_out;
+	 
+    pipeline_CPU dut(
+        .clk(clk),
+        .reset(reset),
+        .iaddr(iaddr),
+        .x31(x31),
+		  .L_regdata_out(L_regdata_out),
+		  .EM_daddr_out(EM_daddr_out),
+		  .MW_drdata_out(MW_drdata_out)
+    );
+	 
+    always #5 clk = ~clk;
+    initial begin
+        clk = 0;
+        reset = 1;
+        #30;
+        reset = 0;
+		  $finish;
+    end
+endmodule
